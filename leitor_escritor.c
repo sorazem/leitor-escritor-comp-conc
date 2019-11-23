6605eaf5156a7b9d131a0e7467b5a58d3c7b972e
Matheus <matheusfcc2010@hotmail.com>
2019-11-23 16:58:42 -0300

    adding tid to leitor func and creating thread leitor

--- leitor_escritor.c
+++ leitor_escritor.c
@@ -14,51 +14,57 @@ int compartilhada; // variavel onde vai ser escrita e lida pelas threads
 sem_t mutex_e, mutex_l, escr, leit; // semáforos
 FILE *arq_log; // arquivo de arq_log
 
-void *le() {
-	sem_wait(&leit);
-	sem_wait(&mutex_l);
-	l++;
-	if(l==1) sem_wait(&escr);
-	sem_post(&mutex_l);
-	sem_post(&leit);
+void *le(void *tid) {
+	int id = *(int*)tid;
 
-	sem_wait(&mutex_l);
-	printf("compartilhada = %d\n", compartilhada);
-	sem_post(&mutex_l);
+	while(1) {
+		sem_wait(&leit);
+		sem_wait(&mutex_l);
+		l++;
+		if(l==1) sem_wait(&escr);
+		sem_post(&mutex_l);
+		sem_post(&leit);
 
-	sem_wait(&mutex_l);
-	l--;
-	if(l==0) sem_post(&escr);
-	sem_post(&mutex_l);
-	
+		sem_wait(&mutex_l);
+		printf("compartilhada = %d\n", compartilhada);
+		sem_post(&mutex_l);
+
+		sem_wait(&mutex_l);
+		l--;
+		if(l==0) sem_post(&escr);
+		sem_post(&mutex_l);
+	}
+		
 	pthread_exit(NULL);
 }
 
 void *escreve(void* tid) {
 	int id = *(int*) tid;
 
-	sem_wait(&mutex_e);
-	e++;
-	if(e==1) sem_wait(&leit);
-	sem_post(&mutex_e);
-	sem_wait(&escr);
-	
-	printf("Escritor %d vai escrever\n", id);
-	compartilhada = id;
-	printf("%d\n", compartilhada);
+	while(1) {
+		sem_wait(&mutex_e);
+		e++;
+		if(e==1) sem_wait(&leit);
+		sem_post(&mutex_e);
+		sem_wait(&escr);
+		
+		printf("Escritor %d vai escrever\n", id);
+		compartilhada = id;
+		printf("%d\n", compartilhada);
 
-	sem_post(&escr);
-	sem_wait(&mutex_e); 
-	e--;
-	if(e==0) sem_post(&leit);
-	sem_post(&mutex_e);
+		sem_post(&escr);
+		sem_wait(&mutex_e); 
+		e--;
+		if(e==0) sem_post(&leit);
+		sem_post(&mutex_e);
+	}
 	
 	pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-	pthread_t *tid_sis_e;
-	int e, l, num_escritas, num_leituras, *tid_e;
+	pthread_t *tid_sis_e, *tid_sis_l;
+	int e, l, num_escritas, num_leituras, *tid;
 	int i;
 	
 	if(argc < 5) {
@@ -83,24 +89,54 @@ int main(int argc, char *argv[]) {
 	tid_sis_e = malloc(e*sizeof(pthread_t));
 	if(!tid_sis_e) {
 		printf("Erro de malloc no tid_sis_e\n");
+		exit(-1);
 	}
 	
 	for(i = 0; i < e; i++) {
-		tid_e = malloc(sizeof(int));
-		if(!tid_e) {
-			printf("Erro de malloc no tid_e\n");
+		tid = malloc(sizeof(int));
+		if(!tid) {
+			printf("Erro de malloc no tid\n");
 			exit(-1);
 		}
-		*tid_e = i;
+		*tid = i;
 		
-		if(pthread_create(&tid_sis_e[i], NULL, escreve, (void*)tid_e)){
+		if(pthread_create(&tid_sis_e[i], NULL, escreve, (void*)tid)){
 			printf("Erro ao criar a thread %d.\n", i);
+			exit(-1);
+		}
+	}
+
+	tid_sis_l = malloc(l*sizeof(pthread_t));
+	if(!tid_sis_l) {
+		printf("Erro de malloc no tid_sis_l\n");
+		exit(-1);
+	}
+
+	for(i = 0; i < l; i++) {
+		tid = malloc(sizeof(int));
+		if(!tid) {
+			printf("Erro de malloc na tid\n");
+			exit(-1);
+		}
+		*tid = i;
+
+		if(pthread_create(&tid_sis_l[i], NULL, le, (void*)tid)) {
+			printf("Erro ao criar a thread %d.\n", l);
+			exit(-1);
 		}
 	}
 	
 	for(i = 0; i < e; i++) {
 		if(pthread_join(tid_sis_e[i], NULL)) {
-			printf("Erro join\n");
+			printf("Erro join de tid_sis_e\n");
+			exit(-1);
+		}
+	}
+
+	for(i = 0; i < l; i++) {
+		if(pthread_join(tid_sis_l[i], NULL)) {
+			printf("Erro join de tid_sis_l\n");
+			exit(-1);
 		}
 	}
 
