8494ddd1e5421ef0b4f90f9ef77af87b0ac773f4
Matheus <matheusfcc2010@hotmail.com>
2019-12-01 20:20:02 -0300

    adding funcs documentation

--- leitor_escritor.c
+++ leitor_escritor.c
@@ -6,23 +6,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
-#include <time.h>
 #include <string.h>
 
-int e, l;
-int num_escritas, num_leituras;
+int e, l;	// quantidade de escritoras e leitoras
+int num_escritas, num_leituras; // quantidade de numero de escritas e leituras para cada thread
+								// de seu respectivo tipo
 
-int compartilhada = 0; // variavel onde vai ser escrita e lida pelas threads
-pthread_mutex_t mutex_leit, fila_chegada, fila_recurso;
-int leitoras = 0;
+int compartilhada = 0;	// variavel onde vai ser escrita e lida pelas threads
+pthread_mutex_t mutex_leit, fila_chegada, fila_recurso; // mutex para a variavel leitoras,
+														// mutex para a fila de chegada de threads,
+														// mutex para a fila de acesso ao Working Room
+int leitoras = 0;	// conta a quantidade de leitoras que estão lendo
 
-pthread_cond_t bar_leit, bar_escr;
-pthread_mutex_t mutex_bar_leit, mutex_bar_escr;
-int leit_esp = 0, escr_esp = 0;
+pthread_cond_t bar_leit, bar_escr;	// variavel de condição para as barreiras de leitora e escritora
+pthread_mutex_t mutex_bar_leit, mutex_bar_escr; // mutex para as barreiras de leitora e escritora
+int leit_esp = 0, escr_esp = 0; // conta a quantidade de leitoras e escritoras que estão esperando
+								// em suas respectivas barreiras
 
-FILE *arq_log; // arquivo de arq_log
+FILE *arq_log; // arquivo de log da execução
 
+/** 
+ * Barreira das threads leitoras. Uma thread leitora só pode fazer leitura da variável compartilhada 
+ * após todas as outras threads leitoras também lerem. Se não for a última thread a passar pela barreira,
+ * então fica bloqueada. Se sim, então desbloqueia todas as outras threads.
+ * 
+ * @param int	numero identificador da thread que está passando pela barreira
+ */
 void barreiraLeitora(int id) {
 	pthread_mutex_lock(&mutex_bar_leit);
 	fprintf(arq_log, "LeitoraEstaNaBarreira(%d)\n", id);
@@ -37,6 +46,16 @@ void barreiraLeitora(int id) {
 	pthread_mutex_unlock(&mutex_bar_leit);
 }
 
+/**
+ * Função para a thread leitora garantir acesso ao Working Room para si e outras threads leitoras. Uma
+ * thread leitora só pode acessar se não tiver nenhuma thread escritora trabalhando. Se não tiver 
+ * nenhuma thread no Working Room, então a leitora garante acesso para si e outras leitoras. Se tiver
+ * uma escritora e ninguém pediu acesso, então pede e espera pelo término da escritora, caso contrário
+ * espera na fila de chegada. Se tiver leitoras e nenhuma escritora pediu acesso, então acessa, caso
+ * contrário espera na fila de chegada.
+ * 
+ * @param int	numero identificador da thread que está passando
+ */
 void entraLeitora(int id) {
 	fprintf(arq_log, "LeitoraEntrouNaFila(%d)\n", id);
 	pthread_mutex_lock(&fila_chegada);
@@ -50,6 +69,12 @@ void entraLeitora(int id) {
 	pthread_mutex_unlock(&mutex_leit);
 }
 
+/**
+ * Função da saída do Working Room da thread leitora. Se for a última leitora trabalhando, então
+ * libera o acesso do Working Room para a escritora.
+ * 
+ * @param int	numero identificador da thread que está passando
+ */
 void saiLeitora(int id) {
 	pthread_mutex_lock(&mutex_leit);
 	leitoras--;
@@ -59,6 +84,13 @@ void saiLeitora(int id) {
 	pthread_mutex_unlock(&mutex_leit);
 }
 
+/**
+ * Função principal da thread leitora. Ela lê o conteúdo da variável compartilhada e escreve em seu
+ * respectivo arquivo de log. Ela escreve uma quantidade de [num_leituras] vezes, definida na entrada
+ * do programa.
+ * 
+ * @param int	numero identificador da thread leitora
+ */
 void *Le(void *tid) {
 	int i = 0;
 	int id = *(int *) tid;
@@ -78,7 +110,7 @@ void *Le(void *tid) {
 
 		entraLeitora(id);
 
-        //printf("Leitora %d lendo compartilhada = %d, i = %d\n", id, compartilhada, i);
+        // Working Room
 		fprintf(arq_log, "Le(%d)\n", compartilhada);
 		putc(compartilhada + '0', arq_leitor);
 
@@ -92,6 +124,13 @@ void *Le(void *tid) {
 	pthread_exit(NULL);
 }
 
+/** 
+ * Barreira das threads escritoras. Uma thread escritora só pode escrever na variável compartilhada 
+ * após todas as outras threads escritoras também escreverem. Se não for a última thread a passar 
+ * pela barreira, então fica bloqueada. Se sim, então desbloqueia todas as outras threads.
+ * 
+ * @param int	numero identificador da thread que está passando pela barreira
+ */
 void barreiraEscritora(int id) {
 	pthread_mutex_lock(&mutex_bar_escr);
 	fprintf(arq_log, "EscritoraEstaNaBarreira(%d)\n", id);
@@ -106,6 +145,15 @@ void barreiraEscritora(int id) {
 	pthread_mutex_unlock(&mutex_bar_escr);
 }
 
+/**
+ * Função para a thread escritora garantir acesso ao Working Room para si. Uma thread escritora só 
+ * pode acessar se e somente se não tiver nenhuma outra thread trabalhando. Se não tiver nenhuma 
+ * thread no Working Room, então a escritora garante acesso para si. Se tiver uma escritora ou
+ * leitoras e ninguém pediu acesso, então pede e espera pelo término da thread que estiver
+ * trabalhando, caso contrário espera na fila de chegada.
+ * 
+ * @param int	numero identificador da thread que está passando
+ */
 void entraEscritora(int id) {
 	fprintf(arq_log, "EscritoraEntrouNaFila(%d)\n", id);
 	pthread_mutex_lock(&fila_chegada);
@@ -114,11 +162,23 @@ void entraEscritora(int id) {
 	pthread_mutex_unlock(&fila_chegada);
 }
 
+/**
+ * Função da saída do Working Room da thread escritora. Libera o acesso do Working Room para qualquer
+ * thread.
+ * 
+ * @param int	numero identificador da thread que está passando
+ */
 void saiEscritora(int id) {
 	//fprintf(arq_log, "SaiEscritora(%d)\n", id);
 	pthread_mutex_unlock(&fila_recurso);
 }
 
+/**
+ * Função principal da thread escritora. Ela escreve na variável compartilhada o seu número
+ * identificador. Ela escreve uma quantidade de [num_escritas] vezes, definida na entrada do programa.
+ * 
+ * @param int	numero identificador da thread leitora
+ */
 void *Escreve(void* tid) {
 	int id = *(int*) tid;
 	int i = 0;
@@ -128,7 +188,7 @@ void *Escreve(void* tid) {
 
 		entraEscritora(id);
         
-        //printf("Escritor %d vai escrever seu id, i = %d\n", id, i);
+        // Working Room
 		fprintf(arq_log, "Escreve(%d)\n", id);
 		compartilhada = id;
 
@@ -140,13 +200,21 @@ void *Escreve(void* tid) {
 	pthread_exit(NULL);
 }
 
-int main(int argc, char *argv[]) {
-	pthread_t *tid_sis_e, *tid_sis_l;
-	int *tid;
-	int i;
 
-	char ch;
-	FILE *source;
+/**
+ * Para execução do leitor-escritor com ausência de inanição entre as threads, utilizamos um mutex 
+ * para uma 'fila de chegada' que indica as threads que tem interesse em acessar ao recurso e um outro
+ * mutex para garantir o acesso exclusivo ao working room, onde ocorre a execução dos trabalhos das 
+ * threads, preservando os requisitos do problema. O fluxo da execução está descrito nas documentações
+ * de cada função. 
+ */
+int main(int argc, char *argv[]) {
+	pthread_t *tid_sis_e, *tid_sis_l; // threads escritoras e leitoras, respectivamente
+	int *tid; // numero identificador de cada thread
+	int i; // variavel iteradora
+
+	char ch; // variavel para copiar cada char do auxiliar.py no arquivo de log
+	FILE *source; // arquivo auxiliar.py
 	
 	if(argc < 5) {
 		fprintf(stderr, "Digite: %s <quantidade de leitores> <quantidade de escritores> <numero de leituras> <numero de escrituras> <arquivo de log>.\n", argv[0]);
@@ -189,8 +257,6 @@ int main(int argc, char *argv[]) {
 		printf("Erro de malloc no tid_sis_e\n");
 		exit(-1);
 	}
-
-
 	
 	tid_sis_l = malloc(l*sizeof(pthread_t));
 	if(!tid_sis_l) {
