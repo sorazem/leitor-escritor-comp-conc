f8fc5cf537001788f81923989bc3bd290e0c7bc2
Stephanie <sorazemhr@gmail.com>
2019-12-01 20:03:39 -0200

    adding exit and log marks

--- auxiliar.py
+++ auxiliar.py
@@ -13,6 +13,7 @@ def EscritoraPedindoAcessoRecurso(id):
 	global fila_chegada, fila_recurso
 	fila_chegada = fila_chegada - 1
 	fila_recurso = fila_recurso + 1
+	print("Escritora pediu acesso e deve ser a proxima a executar")
 	
 def LeitoraEntrouNaFila(id):
 	global fila_chegada
@@ -22,6 +23,7 @@ def LeitoraPedindoAcessoRecurso(id):
 	global fila_chegada, fila_recurso
 	fila_chegada = fila_chegada - 1
 	fila_recurso = fila_recurso + 1
+	print("Leitora pediu acesso e deve ser a proxima a executar")
 
 def EscritoraEstaNaBarreira(id):
 	global escr_esp
@@ -35,6 +37,7 @@ def BroadcastLeitoras(l):
 	global leit_esp
 	if leit_esp != l:
 		print("ERRO--- Broadcast. Thread leitora sinalizou no momento errado.")
+		exit(1)
 	else:
 		leit_esp = 0
 
@@ -42,6 +45,7 @@ def BroadcastEscritoras(e):
 	global escr_esp
 	if escr_esp != e:
 		print("ERRO--- Broadcast. Thread escritora sinalizou no momento errado.")
+		exit(1)
 	else:
 		escr_esp = 0
 	
@@ -50,21 +54,27 @@ def Escreve(x):
 	global compartilhada, fila_recurso, recurso_ocupado
 	if recurso_ocupado == 1:
 		print("ERRO--- Thread acessou o recurso quando nao devia.")
+		exit(1)
 	else:
 		fila_recurso = fila_recurso - 1
 		recurso_ocupado = 1
 		compartilhada = x
+		print("Escritora escreveu com sucesso")
 	recurso_ocupado = 0
 
 def Le(x):
 	global compartilhada, fila_recurso, recurso_ocupado
 	if recurso_ocupado == 1:
 		print("ERRO--- Thread acessou o recurso quando nao devia.")
+		exit(1)
 	else:
 		fila_recurso = fila_recurso - 1
 		recurso_ocupado = 1
 	
 	if compartilhada != x:
 		print("ERRO--- Leitora leu o valor errado.")
+		exit(1)
+	else:
+		print("Leitora leu com sucesso")
 	recurso_ocupado = 0
 	
